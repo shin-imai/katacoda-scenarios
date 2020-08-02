6cf5346c51651cf868d609ed1d5a0a529e35b553
Shin <linuxtatsujin@yahoo.com>
2020-08-02 10:01:34 +0000

    Course created

--- Pods/scripts/cleanup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-(kubectl get po --no-headers | awk '{print $1}' | xargs kubectl delete po --force --grace-period=0) &>/dev/null
--- Pods/scripts/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in $(ls /root/assets);do cat /root/assets/$i > /root/samples/$i;done
